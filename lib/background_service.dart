28c2f742b2e170f7b772a5b1e6d89d7371d97983
Mulori <murilogarcia.hr@gmail.com>
2024-04-25 23:10:18 -0300

    primeiro commit

---
+++ lib/background_service.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'notification.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+
+  await service.configure(
+      iosConfiguration: IosConfiguration(
+        autoStart: true,
+        onForeground: onStart,
+        onBackground: onIosBackground,
+      ),
+      androidConfiguration: AndroidConfiguration(
+          onStart: onStart, isForegroundMode: true, autoStart: true));
+}
+
+@pragma("vm:entry-point")
+Future<bool> onIosBackground(ServiceInstance service) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+
+  return true;
+}
+
+@pragma("vm:entry-point")
+void onStart(ServiceInstance service) {
+  int counter = 0;
+  DartPluginRegistrant.ensureInitialized();
+
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+    });
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+    });
+  }
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+
+  Timer.periodic(const Duration(seconds: 3), (timer) async {
+    if (service is AndroidServiceInstance) {
+      if (await service.isForegroundService()) {
+        print('Rodou em foreground ${counter++}');
+      }
+    }
+
+    print('serviço está rodando em background ${counter++}');
+
+    service.invoke('update');
+  });
+}
